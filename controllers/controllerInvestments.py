1bb54aa42f35579ab1397dd759f509c89717af71
Leonardo.C <leocarminhoto@gmail.com>
2024-07-14 21:00:57 -0300

    Adding Models

---
+++ controllers/controllerInvestments.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../models'))
+sys.path.append(module_path)
+import InvestmentsModel
+
+
+
+class Investment():
+    
+    def __init__(self,params) -> None:
+        
+       self.params = params
+    
+    def createInvestments(self) -> None:
+
+        createdLeaves = InvestmentsModel.Investments(self.params).createInvestmentsDB()
+        if(createdLeaves):
+            print('Investimento Criada!!!')
+        
+        
+
+
+
+params = {
+    "name_Investiments": "salario",
+    "type_investments": "100",
+    "value": "1",
+    "profitability": "1",
+    "id_user": "1"
+    
+}
+
+Investment(params).createInvestments()
---
+++ controllers/controllerLeave.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../models'))
+sys.path.append(module_path)
+import LeavesModel
+
+
+
+class Leave():
+    
+    def __init__(self,params) -> None:
+        
+       self.params = params
+    
+    def createLeaves(self) -> None:
+
+        createdLeaves = LeavesModel.Leaves(self.params).createLeavesDB()
+        if(createdLeaves):
+            print('Saida Criada!!!')
+        
+        
+
+
+
+params = {
+    "nameLeave": "salario",
+    "value": "100",
+    "installments": "1",
+    "pays_installments": "1",
+    "pays_finish": "1",
+    "id_user": "1"
+    
+}
+
+Leave(params).createLeaves()
