2345d8e1126dc3b1ccc9b67f71bc78ae7f151bce
Leonardo.C <leocarminhoto@gmail.com>
2024-08-12 21:29:28 -0300

    Add Model|Controller Select ALL TYPES

--- models/TypeEntriesModel.py
+++ models/TypeEntriesModel.py
@@ -38,3 +38,32 @@ class TypeEntries:
                     return "Error"
                 finally:
                     self.conn.close()
+    def selectAllItens(self)-> str:
+            retries = 5
+            while retries >0:
+                try:
+                    sql = f'SELECT type_name FROM typeEntries' 
+                    querryExecute = self.cursor.execute(sql)
+                    self.conn.commit()
+
+                    if querryExecute:
+                        self.conn.commit()
+                        return "OK"
+                    else:
+                        return "NOK"
+                    
+                except sqlite3.OperationalError as err:
+                    if 'database is locked' in str(err):
+                        print("Database is locked, retrying...")
+                        retries -= 1
+                        time.sleep(1)  
+                    else:
+                        print(f"An operational error occurred: {err}")
+                        self.conn.rollback()
+                        return "Error" 
+                except sqlite3.Error as err:
+                    print(f"An error occurred while inserting data: {err}")
+                    self.conn.rollback()
+                    return "Error"
+                finally:
+                    self.conn.close()
