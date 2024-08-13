80a807c57a2e496704d8727fbc51d9335c7b4a7c
lului07 <luciocoffe746@gmail.com>
2024-08-13 20:38:54 -0300

    process not complete

---
+++ controllers/processosContabeis.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+class processosContabeis:
+    def __init__(self, params) -> None:
+        self.params = params
+
+    def desconto(self) -> int:
+
+        print("descontos:")
+        print("\tolá")
+
+        # desconto de receita
+        while True:
+            i = 0
+            receitas -= custos[i]
+            print(f"{custos[i]}")
+            
+
+
+# dicionário com valores à serem descontados das receitas
+params = {
+    "name_Investiments": "arigato_gozarmais",
+    "type_investments":"BDR",
+    "value":"5400",
+    "profitability":"200",
+    "id_user":"5",
+    "id_investimentos":"4"
+}
+
+processosContabeis.desconto()
