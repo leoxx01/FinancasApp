89e02b11a3c02232a1d02126bd2bdb8009021c83
luci746 <lucio32bits@gmail.com>
2024-08-22 20:46:02 -0300

    processosContábeis 30/100

--- controllers/processosContabeis.py
+++ controllers/processosContabeis.py
@@ -12,61 +12,92 @@ import LeavesModel
 
 class processosContabeis:
     def __init__(self, paramsEntry, paramsLeave) -> None:
-        self.paramsEntry = paramsEntry
-        self.paramsLeave = paramsLeave
+        self.receita = paramsEntry
+        self.custos = paramsLeave
 
-        # Data da operação
-        data = datetime.now()
-        data = str(data)
-        data = data.split('-')
-        self.dataInicio = f"{data[0]}-{data[1]}-01"
-        self.dataFim = f"{data[0]}-{data[1]}-{int(str(data[2])[0:2])+1}" 
+        ###  Esta aplicação só poderá ser ligada ao modal, quando as saídas estiverem prontas !!!
 
-        # importar as entradas
-        self.receita = EntriesModel.Entries(self.paramsEntry).getItemById(self.dataInicio, self.dataFim)
+#        # Data da operação
+#        data = datetime.now()
+#        data = str(data)
+#        data = data.split('-')
+#        self.dataInicio = f"{data[0]}-{data[1]}-01"
+#        self.dataFim = f"{data[0]}-{data[1]}-{int(str(data[2])[0:2])+1}" 
+#
+#        # importar as entradas
+#        self.receita = EntriesModel.Entries(self.paramsEntry).getItemById(self.dataInicio, self.dataFim)
+#
+#        # importar as saídas
+#        self.gastos = LeavesModel.Leaves(self.paramsLeave).getItemById(self.dataInicio, self.dataFim)
 
-        # importar as saídas
-        self.gastos = LeavesModel.Leaves(self.paramsLeave).getItemById(self.dataInicio, self.dataFim)
-
-
-
-
+        
     def desconto(self) -> None:
 
         receita = self.receita
+        custos = self.custos
 
-        if(receita):
-            print("oi")
-        else:
-            print("tchau")
+        print(receita[0]['valor'])
+        print(custos[0]['value'])
 
+        print('\n')
+
+        # total receitas
         rec = []
-        for resultados in receita:
-            rec.append(float(resultados[2]))
-            print(rec)
+        for valores in receita:
+            cont = 0
+            rec.append(float(valores['valor']))
+            cont += 1
+        
+        receitaTotal = sum(rec)
+        print(receitaTotal)
 
-        soma = sum(rec)
+        # total custos
+        cus = []
+        for valores in custos:
+            cont = 0
+            cus.append(float(valores['value']))
+            cont += 1
+        
+        custoTotal = sum(cus)
+        print(custoTotal)
 
-        if (soma):
-            print(f"Receita Liquidada! \n valor líquido: {soma}")
+        # desconto final
+        
 
-
-# dicionário com valores à serem descontados das receitas
-paramsEntry = {
-    "nome_entrada": "Salario",
-    "valor": 1000,
-    "id_user": 14,
-    "id_entries": 1
-}
-
-paramsLeave = {
-    "nameLeave": "carro",
-    "value": 10000,
-    "installments": 1,
-    "pays_installments": 1,
-    "pays_finish": 1,
-    "id_user": 14,
-    "id_leave": 2
-}
+# dicionário com valores à serem descontados das receitas (exemplo)
+paramsEntry = [
+        {
+        "nome_entrada": "Salario",
+        "valor": 1000,
+        "id_user": 14,
+        "id_entries": 1
+    },
+        {
+        "nome_entrada": "Salario",
+        "valor": 1030,
+        "id_user": 14,
+        "id_entries": 1
+    }
+]
+paramsLeave = [
+        {
+        "nameLeave": "carro",
+        "value": 10000,
+        "installments": 1,
+        "pays_installments": 1,
+        "pays_finish": 1,
+        "id_user": 14,
+        "id_leave": 2
+    },
+        {
+        "nameLeave": "carro",
+        "value": 2000,
+        "installments": 1,
+        "pays_installments": 1,
+        "pays_finish": 1,
+        "id_user": 14,
+        "id_leave": 2
+    }
+]
 
 processosContabeis(paramsEntry, paramsLeave).desconto()
